932b616116f6fc95fa33153d69c7a9261043bdba
Marco Introini <89445565+marco-introini@users.noreply.github.com>
2022-11-18 21:54:18 +0100

    add first step

---
+++ tests/SplunkFormatterTest.php
@@ -0,0 +1,22 @@
+<?php
+
+use Mintdev\Monolog\Formatter\SplunkLineFormatter;
+use Monolog\Handler\TestHandler;
+use Monolog\Level;
+use Monolog\Logger;
+
+use function Pest\Faker\faker;
+
+beforeEach(function (){
+    $this->splunkFormatter = new SplunkLineFormatter();
+    $this->handler = new TestHandler(Level::Debug);
+    $this->handler->setFormatter($this->splunkFormatter);
+    $log = new Logger('test');
+    $log->pushHandler($this->handler);
+});
+
+test('a basic string works with no conversions', function () {
+    $str = faker()->text;
+    $converted = $this->splunkFormatter->publicConvertToString($str);
+    expect($converted)->toEqual($str);
+});
